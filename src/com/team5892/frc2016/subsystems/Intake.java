1d2aa8c96ee1cb1eda5e76c696e0f859baa80258
Andres Abrego <andres.r.abrego@gmail.com>
2016-03-19 19:59:53 -0500

    Populated the Intake class

--- src/com/team5892/frc2016/subsystems/Intake.java
+++ src/com/team5892/frc2016/subsystems/Intake.java
@@ -1,18 +1,39 @@
 package com.team5892.frc2016.subsystems;
 
+import com.androb4.frc.lib.CheesySpeedController;
+import com.team5892.frc2016.Robot;
+import com.team5892.frc2016.RobotMap;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Intake extends Subsystem {
+	
+	private CheesySpeedController m_intake = new CheesySpeedController(
+			new VictorSP(RobotMap.pwm_intake_1),
+			Robot.pdp,
+			RobotMap.pdp_intake_1);
     
+	private DigitalInput ball_sensor = new DigitalInput(RobotMap.di_ball_sensor);
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void setUnsafePower(double speed) {
+    	m_intake.set(speed);
+    }
+    
+    public boolean isBallPresent() {
+    	return ball_sensor.get();
     }
 }
 
