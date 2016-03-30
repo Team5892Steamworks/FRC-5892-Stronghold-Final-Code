911a4fd879d1fe63c2c0b6a2943d4a3510c8ebee
Andres Abrego <andres.r.abrego@gmail.com>
2016-03-30 14:21:11 -0500

    Updated PDP ports. Updated hanger arm constants. Added gyro constants.

--- src/com/team5892/frc2016/RobotMap.java
+++ src/com/team5892/frc2016/RobotMap.java
@@ -19,17 +19,17 @@ public class RobotMap {
 	public static int pwm_intake_1 = 8;
 	
 	// PDP
-	public static int pdp_drive_left_1 = 0;
-	public static int pdp_drive_left_2 = 1;
+	public static int pdp_drive_left_1 = 14;
+	public static int pdp_drive_left_2 = 13;
 	public static int pdp_drive_right_1 = 2;
 	public static int pdp_drive_right_2 = 3;
-	public static int pdp_shooter_left = 4;
-	public static int pdp_shooter_right = 5;
-	public static int pdp_intake_1 = 6;
-	public static int pdp_hanger_pivot_left = 7;
-	public static int pdp_hanger_pivot_right = 8;
-	public static int pdp_hanger_winch_left = 9;
-	public static int pdp_hanger_winch_right = 10;
+	public static int pdp_shooter_left = 15;
+	public static int pdp_shooter_right = 12;
+	public static int pdp_intake_1 = 9;
+	public static int pdp_hanger_pivot_left = 0;
+	public static int pdp_hanger_pivot_right = 1;
+	public static int pdp_hanger_winch_left = 10;
+	public static int pdp_hanger_winch_right = 11;
 	
 	// DIO
 	public static int di_hanger_winch_encoder_left_a = 0;
@@ -38,7 +38,7 @@ public class RobotMap {
 	public static int di_hanger_winch_encoder_right_b = 3;
 
 	// AI
-	public static int ai_shooter_tach = 0;
+	public static int ai_gyro_drive = 0;
 	public static int ai_hanger_angle_left = 1;
 	public static int ai_hanger_angle_right = 2;
 	public static int ai_intake_ball_sensor = 3;
@@ -52,17 +52,20 @@ public class RobotMap {
 	public static double kTachAnalogTriggerLow = 0;
 	public static double kTachAnalogTriggerHigh = 0;
 	
-	public static double kHangerLeftPivotFullRange = -90.0;
-	public static double kHangerRightPivotFullRange = -147.541;
-	public static double kHangerLeftPivotOffset = 90.0;
-	public static double kHangerRightPivotOffset = 142.377;
+	public static double kHangerLeftPivotFullRange = -142.2893482;
+	public static double kHangerLeftPivotOffset = 141.9356121;
+	public static double kHangerRightPivotFullRange = 154.3103448;
+	public static double kHangerRightPivotOffset = -37.7689552;
 	
 	public static double kHangerLeftBottomLimitDegrees = 2;
 	public static double kHangerLeftUpLimitDegrees = 100;
 	public static double kHangerRightBottomLimitDegrees = 2;
 	public static double kHangerRightUpLimitDegrees = 100;
 	
-	public static int kIntakeBallThreshold = 1000;
+	public static double kIntakeBallThreshold = 3.5;
+	
+	public static int kDriveGyroAccumulatorCenter = 0;
+	public static double kDriveGyroAccumulatorOffset = 0.0;
 	
 	public static double kFlywheelP = 0;
 	public static double kFlywheelI = 0;
