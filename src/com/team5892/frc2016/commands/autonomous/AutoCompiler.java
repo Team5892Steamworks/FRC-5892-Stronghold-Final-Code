0a10e451dd3c89bf5496644bd5876ef794925635
Andres Abrego <andres.r.abrego@gmail.com>
2016-04-07 05:28:05 -0500

    Half completed AutoCompiler class, functional

--- src/com/team5892/frc2016/commands/autonomous/AutoCompiler.java
+++ src/com/team5892/frc2016/commands/autonomous/AutoCompiler.java
@@ -1,6 +1,9 @@
 package com.team5892.frc2016.commands.autonomous;
 
 import com.team5892.frc2016.Robot;
+import com.team5892.frc2016.commands.ShooterSet;
+import com.team5892.frc2016.commands.intake.IntakeSet;
+import com.team5892.frc2016.subsystems.Intake;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -8,54 +11,53 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoCompiler extends CommandGroup {
 	
 	public static AutoCompiler auto;
+	private int position;
+	private Routines[] routines = {null, null, null};
 	
 	public AutoCompiler() {
+		position = (int) Robot.autoPosition.getSelected();
+		routines[0] = (Routines) Robot.autoRoutine1.getSelected();
+		routines[1] = (Routines) Robot.autoRoutine2.getSelected();
+		routines[2] = (Routines) Robot.autoRoutine3.getSelected();
 		
 		// Do stuff with the auto position
 		
-		if(Robot.autoRoutine1.getSelected() == Routines.AutoCrossSimple) {
-			addSequential(new AutoCrossSimple());
+		if(routines[0] == Routines.CrossSimple) {
+			addSequential(new CrossSimple());
 		}
-		else if(Robot.autoRoutine1.getSelected() == Routines.CrossLowBar) {
+		else if(routines[0] == Routines.CrossLowBar) {
 			addSequential(new CrossLowBar());
 		}
-		else if(Robot.autoRoutine1.getSelected() == Routines.CrossCDF) {
-			addSequential(new CrossLowBar());
+		else if(routines[0] == Routines.CrossCDF) {
+			addSequential(new AutoDoNothing());
 		}
 		
-		if(Robot.autoRoutine2.getSelected() == Routines.HighGoal) {
-			addSequential(new AutoCrossSimple());
+		if(routines[1] == Routines.HighGoal) {
+			addParallel(new ShooterSet());
+			addSequential(new DriveToHighGoal(position));
+			addSequential(new IntakeSet(Intake.State.Intake));
 		}
-		else if(Robot.autoRoutine2.getSelected() == Routines.LowGoal) {
-			addSequential(new AutoCrossSimple());
+		else if(routines[1] == Routines.LowGoal) {
+			addSequential(new DriveToLowGoal(position));
+			addSequential(new IntakeSet(Intake.State.Exhaust));
 		}
-		else if(Robot.autoRoutine2.getSelected() == Routines.Return) {
-			addSequential(new AutoCrossSimple());
+		else if(routines[1] == Routines.Return) {
+			addSequential(new AutoDoNothing());
 		}
-		else if(Robot.autoRoutine2.getSelected() == Routines.None) {
-			addSequential(new AutoCrossSimple());
+		else if(routines[1] == Routines.None) {
+			addSequential(new AutoDoNothing());
 		}
 		
-		if(Robot.autoRoutine3.getSelected() == Routines.None) {
-			addSequential(new AutoCrossSimple());
+		if(routines[2] == Routines.None) {
+			addSequential(new AutoDoNothing());
 		}
-		else if(Robot.autoRoutine3.getSelected() == Routines.Return) {
-			addSequential(new AutoCrossSimple());
+		else if(routines[2] == Routines.Return) {
+			addSequential(new AutoDoNothing());
 		}
 	}
-	
-	public static Command[] routines = {null, null, null};
 	
 	public enum Routines {
-		AutoCrossSimple, CrossLowBar, CrossCDF, Spybot, HighGoal, LowGoal, Return, None
-	}
-
-	public static void addRoutine(Command routine) {
-		for(Command r: routines) {
-			if(r == null) {
-				r = routine;
-			}
-		}
+		CrossSimple, CrossLowBar, CrossCDF, Spybot, HighGoal, LowGoal, Return, None
 	}
 	
 	public static Command getAuto() {
