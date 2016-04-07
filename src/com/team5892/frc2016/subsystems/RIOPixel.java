54d8e9a1aece6dfd1b4ea09670c69767d360db45
Andres Abrego <andres.r.abrego@gmail.com>
2016-04-06 20:20:35 -0500

    Fixed RIOPixel error when no device is present

--- src/com/team5892/frc2016/subsystems/RIOPixel.java
+++ src/com/team5892/frc2016/subsystems/RIOPixel.java
@@ -8,7 +8,30 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class RIOPixel extends Subsystem {
     
-	private SerialPort rioPixelSerial = new SerialPort(115200, SerialPort.Port.kUSB);
+	private SerialPort rioPixelSerial;
+	
+	public enum Lights {
+		noComm(0), disabled(1), autoIntake(2), spinUp(3), shoot(4), scalingMode(5), scaled(6);
+		
+		private int value;
+		
+		private Lights(int value) {
+		      this.value = value;
+		}
+
+	    public int getValue() {
+	      return this.value;
+	    }
+	}
+	
+	public RIOPixel() {
+		try {
+			rioPixelSerial = new SerialPort(115200, SerialPort.Port.kUSB);
+		}
+		catch(Exception e) {
+			System.err.println(e);
+		}
+	}
 	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -16,6 +39,10 @@ public class RIOPixel extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void setLights(Lights pattern) {
+    	
     }
 }
 
