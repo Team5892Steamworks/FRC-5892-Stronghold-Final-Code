6f0cdc4de4c8c4259a81f175aadaa7e2b9b58a02
Andres Abrego <andres.r.abrego@gmail.com>
2016-04-08 03:57:20 -0500

    Added complete version 1 of RIOPixel firmware

--- RIOPixel_Firmware/RIOPixel_Firmware.ino
+++ RIOPixel_Firmware/RIOPixel_Firmware.ino
@@ -5,8 +5,8 @@
 #define STRIP_RIGHT_DATA_PIN 7
 
 #define RING_NUM_LEDS 10
-#define STRIP_LEFT_NUM_LEDS 20
-#define STRIP_RIGHT_NUM_LEDS 20
+#define STRIP_LEFT_NUM_LEDS 19
+#define STRIP_RIGHT_NUM_LEDS 19
 
 CRGB ring_leds[RING_NUM_LEDS];
 CRGB strip_left_leds[STRIP_LEFT_NUM_LEDS];
@@ -24,10 +24,10 @@ void setup() {
   setAllToBlack();
 }
 
-unsigned long currentTime;
+unsigned long currentTime = 0;
 unsigned long prevTime = 0;
 unsigned long interval = 15;
-int controlByte;
+byte controlByte;
 
 /*
  * Control Bytes
@@ -35,39 +35,47 @@ int controlByte;
  *  1. Disabled
  *  2. Standby
  *  3. Auto Intake
- *  4. Flywheel spinup
- *  5. Shoot
- *  6. Scale mode
+ *  4. Has ball
+ *  5. Flywheel spinup
+ *  6. Shoot
+ *  7. Scale mode
  */
 
-int i = 0;
+long i = 0;
 
 void loop() {
-  //for (int i = 0; i < STRIP_LEFT_NUM_LEDS + 2; i++) {
-
   if (Serial.available() > 0) {
-    controlByte = Serial.read();
+    controlByte = Serial.read() - '0';
     Serial.println(controlByte);
     if (controlByte == 0) {
       interval = 15;
     }
     else if (controlByte == 1) {
-      interval = 15;
+      interval = 10;
     }
     else if (controlByte == 2) {
-
+      i = 0;
+      interval = 60;
     }
     else if (controlByte == 3) {
-
+      i = 0;
+      interval = 20;
     }
     else if (controlByte == 4) {
-
+      i = 0;
+      interval = 60;
     }
     else if (controlByte == 5) {
-      interval = 100;
+      i = 0;
+      interval = 15;
     }
     else if (controlByte == 6) {
-
+      i = 0;
+      interval = 100;
+    }
+    else if (controlByte == 7) {
+      i = 0;
+      interval = 100;
     }
   }
 
@@ -79,28 +87,74 @@ void loop() {
 
     }
     else if (controlByte == 1) {
-
+      patternDisabled(i);
+      i = i + 1;
+      if (i == 255) {
+        i = 0;
+      }
     }
     else if (controlByte == 2) {
-
+      patternHasBall(i);
+      i = i + 1;
+      if (i == STRIP_LEFT_NUM_LEDS) {
+        i = 0;
+      }
     }
     else if (controlByte == 3) {
-
+      patternAutoIntake(i);
+      i = i + 1;
+      if (i == STRIP_LEFT_NUM_LEDS) {
+        i = 0;
+      }
     }
     else if (controlByte == 4) {
-      patternFlywheelStartup(i);
-      i++;
-      if (i <= STRIP_LEFT_NUM_LEDS) i = 0;
+      patternHasBall(i);
+      i = i + 1;
+      if (i == STRIP_LEFT_NUM_LEDS) {
+        i = 0;
+      }
     }
     else if (controlByte == 5) {
+      patternFlywheelStartup(i);
+      i = i + 1;
+      if (i == STRIP_LEFT_NUM_LEDS) {
+        i = 0;
+      }
+    }
+    else if (controlByte == 6) {
       i++;
       patternShoot(i);
     }
-    else if (controlByte == 6) {
-
+    else if (controlByte == 7) {
+      patternScaleMode(i);
+      i = i + 1;
     }
   }
-  //}
+}
+
+/////////////////// Patterns
+
+void patternDisabled(int i) {
+  setAll(i, i/5, 0);
+}
+
+void patternAutoIntake(int i) {
+  if (i == 0) {
+    setAll(255, 50, 0);
+  }
+  setPixel(STRIP_LEFT_NUM_LEDS - i - 1, 255, 255, 255);
+  FastLED.show();
+}
+
+void patternHasBall(int i) {
+  setPixel(i - 2, 255, 50, 0);
+  setPixel(i - 1, 0, 127, 255);
+  setPixel(i, 0, 127, 255);
+
+  setPixel(STRIP_LEFT_NUM_LEDS - i, 255, 50, 0);
+  setPixel(STRIP_LEFT_NUM_LEDS - i - 1, 0, 127, 255);
+  setPixel(STRIP_LEFT_NUM_LEDS - i - 2, 0, 127, 255);
+  FastLED.show();
 }
 
 void patternFlywheelStartup(int i) {
@@ -115,31 +169,48 @@ void patternFlywheelStartup(int i) {
   // delay 15
 }
 
-/////////////////// Patterns
-
-void patternAutotIntake() {
-
-}
-
 void patternShoot(int i) {
   if (i % 2) {
     for (int i = 0; i < STRIP_LEFT_NUM_LEDS; i++) {
-      strip_left_leds[i][0] = 255;
-      strip_left_leds[i][1] = 50;
-      strip_left_leds[i][2] = 0;
+      strip_left_leds[i][0] = strip_left_leds[i][0] = 255;
+      strip_left_leds[i][1] = strip_left_leds[i][1] = 50;
+      strip_left_leds[i][2] = strip_left_leds[i][2] = 0;
     }
   }
   else {
     for (int i = 0; i < STRIP_LEFT_NUM_LEDS; i++) {
-      strip_left_leds[i][0] = 255;
-      strip_left_leds[i][1] = 255;
-      strip_left_leds[i][2] = 255;
+      strip_left_leds[i][0] = strip_left_leds[i][0] = 255;
+      strip_left_leds[i][1] = strip_left_leds[i][1] = 255;
+      strip_left_leds[i][2] = strip_left_leds[i][2] = 255;
     }
   }
   FastLED.show();
 }
 
+void patternScaleMode(int i) {
+  if(i%4 == 0) {
+    setAll(255, 50, 0);
+  }
+  else if(i%4 == 1) {
+    setAll(255, 255, 255);
+  }
+  else if(i%4 == 2) {
+    setAll(0, 127, 255);
+  }
+  else if(i%4 == 3) {
+    setAll(0, 255, 0);
+  }
+}
+
 /////////////////////
+void setAll(int r, int g, int b) {
+  for (int i = 0; i < STRIP_LEFT_NUM_LEDS; i++) {
+    strip_left_leds[i].setRGB(r, g, b);
+    strip_right_leds[i].setRGB(r, g, b);
+  }
+  FastLED.show();
+}
+
 
 void setAllToBlack() {
   for (int i = 0; i < STRIP_LEFT_NUM_LEDS; i++) {
