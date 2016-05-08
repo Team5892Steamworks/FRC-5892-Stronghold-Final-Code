d7d7ee624111a7539308f15ba1b4646784866b40
Andres Abrego <andres.r.abrego@gmail.com>
2016-05-08 16:45:12 -0500

    Increased speed and time for CrossSimple

--- src/com/team5892/frc2016/commands/autonomous/CrossSimple.java
+++ src/com/team5892/frc2016/commands/autonomous/CrossSimple.java
@@ -10,6 +10,6 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CrossSimple extends CommandGroup {
     
     public  CrossSimple() {
-        addSequential(new DriveStraightGyro(0.4, 2.0)); // Drive 134 inches. Cross defense
+        addSequential(new DriveStraightGyro(0.7, 3.0)); // Drive 134 inches. Cross defense
     }
 }
